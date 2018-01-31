5ff32ff8f88e484571320d736e1e384b79196670
Fredrik Lindsten <fredrik.lindsten.0438@student.uu.se>
2018-01-31 14:14:09 +0100

    Added alternate rotation

--- Assets/Scripts/scr_skyslugMovement.cs
+++ Assets/Scripts/scr_skyslugMovement.cs
@@ -15,6 +15,8 @@ public class scr_skyslugMovement : MonoBehaviour {
     private float randomDistanceAcceleration = 0;
 
     public bool rotate = false;
+    public bool altrotate = false;
+    public float altrotatemagnitude = 100;
 
     public float attackCooldown = 0;
     private float attackTimer = 0;
@@ -58,6 +60,7 @@ public class scr_skyslugMovement : MonoBehaviour {
             circlePosition = Mathf.Atan2((-playerPosition.normalized).y, (-playerPosition.normalized).x);
             circlePosition += 2 * movement / swarmDistance;
 
+            //TODO This block needs fewer magic values and better handling of when the slug moves out of range
             randomDistanceAcceleration += Random.Range(-1.0f, 1.0f) / 100;
             randomDistance += randomDistanceAcceleration;
             if (randomDistance < swarmDistanceMin || randomDistance > swarmDistanceMax)
@@ -80,6 +83,11 @@ public class scr_skyslugMovement : MonoBehaviour {
         }
         if (rotate)
             transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, destination));
+        if (altrotate) { 
+            transform.rotation = Quaternion.Euler(0, 0, altrotatemagnitude * (destination.normalized*movement).y - 90);
+            if (attacking && rotate)
+                transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, destination));
+        }
         transform.Translate(destination.normalized * movement, Space.World);
         //collision
     }
