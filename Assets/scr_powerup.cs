ad49e3912fa8e29448b95b5453ef4636e41bafdb
Fredrik Lindsten <fredrik.lindsten.0438@student.uu.se>
2018-02-09 19:39:58 +0100

    Playtest scene and temp code

---
+++ Assets/scr_powerup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_powerup : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnDestroy()
+    {
+        scr_utilities.instance.aetherLeft += 10;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(-Time.deltaTime, 0, 0);
+	}
+}
