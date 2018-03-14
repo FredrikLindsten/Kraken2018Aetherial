c4bfefd0385bc88472e7186106def3f7a3781bdf
Matthias Jannidis <matthias.jannidis@gmail.com>
2018-03-14 10:40:38 +0100

    fix and crosshair

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -8,7 +8,7 @@ public class Crosshair : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Cursor.SetCursor(sprite, new Vector2(sprite.width / 2, sprite.height / 2), CursorMode.Auto);
+        Cursor.SetCursor(sprite, new Vector2(sprite.width / 2 + 1, sprite.height / 2 + 1), CursorMode.Auto);
     }
 
     // Update is called once per frame
